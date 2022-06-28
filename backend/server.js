8f2520dd5428e6cf98a83c2187970b4037c49c9e
chaitak-gorai <chaitakgorai@gmail.com>
2022-06-28 15:36:59 +0530

    Backend env setup

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,24 @@
-const express = require('express');
-const products = require('./data/products.js');
-const app = express();
-const cors = require('cors');
-app.use(cors());
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './data/products.js'
+import cors from 'cors'
+dotenv.config()
+const app = express()
+
+app.use(cors())
 app.get('/', (req, res) => {
-  res.send('API is running');
-});
+  res.send('API is running..')
+})
 
 app.get('/api/products', (req, res) => {
-  res.json(products);
-});
+  res.json(products)
+})
 
 app.get('/api/products/:id', (req, res) => {
-  const product = products.find((product) => product._id === req.params.id);
-  res.json(product);
-});
-
-app.listen(5000, () => console.log('Server started on port 5000'));
+  const product = products.find((product) => product._id === req.params.id)
+  res.json(product)
+})
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () =>
+  console.log(`Server started in ${process.env.NODE_ENV} on port ${PORT}`)
+)
