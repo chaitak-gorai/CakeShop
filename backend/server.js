e0f3db83f0f28fa221e7fe61dcc211e667a2ddc1
chaitak-gorai <chaitakgorai@gmail.com>
2022-06-28 23:02:11 +0530

    🗃️ mongoose setup and Schema

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import colors from 'colors'
 import products from './data/products.js'
+
 import cors from 'cors'
 dotenv.config()
 const app = express()
+
+connectDB()
 
 app.use(cors())
 app.get('/', (req, res) => {
@@ -20,5 +25,7 @@ app.get('/api/products/:id', (req, res) => {
 })
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () =>
-  console.log(`Server started in ${process.env.NODE_ENV} on port ${PORT}`)
+  console.log(
+    `Server started in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+  )
 )
