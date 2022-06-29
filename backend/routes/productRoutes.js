a64676249ef0ab2403033f64ba9a07e346c238c0
chaitak-gorai <chaitakgorai@gmail.com>
2022-06-29 11:52:18 +0530

    🔒️ error middlewares

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,38 @@
+import express from 'express'
+import asyncHandler from 'express-async-handler'
+const router = express.Router()
+import Product from '../models/productModel.js'
+
+//@desc fetch all products
+//@route GET /api/products
+//@access Public
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    console.log('GET /api/products'.white.inverse)
+    const products = await Product.find({})
+    res.json(products)
+    console.log('Fetched all products'.green.inverse)
+  })
+)
+
+//@desc fetch single product
+//@route GET /api/products/:id
+//@access Public
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    console.log('GET /api/products/:id'.white.inverse)
+    const product = await Product.findById(req.params.id)
+    if (product) {
+      res.json(product)
+      console.log('Fetched product'.green.inverse)
+    } else {
+      res.status(404)
+      throw new Error('Product not found')
+      console.log('Product not found'.red.inverse)
+    }
+  })
+)
+
+export default router
--- backend/server.js
+++ backend/server.js
@@ -2,27 +2,28 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
-import products from './data/products.js'
-
 import cors from 'cors'
+
+import productRoutes from './routes/productRoutes.js'
+import { errorHandler, notFound } from './middlewares/errorMiddleware.js'
+
 dotenv.config()
 const app = express()
-
 connectDB()
-
 app.use(cors())
+
 app.get('/', (req, res) => {
   res.send('API is running..')
 })
+app.use('/api/products', productRoutes)
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
+//*middleware for no route found
+app.use(notFound)
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((product) => product._id === req.params.id)
-  res.json(product)
-})
+//* This middleware will be used to handle errors
+app.use(errorHandler)
+
+//* Start the server
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () =>
   console.log(
